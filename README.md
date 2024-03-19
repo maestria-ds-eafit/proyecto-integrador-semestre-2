322965b91bd8ce838b2f9975d0436ff19d074195
David Armendariz <david.adrian.a.p@gmail.com>
2024-03-19 17:46:02 -0500

    Adds policies and roles

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# proyecto-integrador-semestre-2
-Proyecto integrador para el segundo semestre de la maestría de ciencia de datos y analítica de EAFIT
+# Proyecto integrador
+
+Proyecto integrador para el segundo semestre de la maestría de ciencia de datos y analítica de EAFIT.
+
+## Configuración para usar AWS EMR
+
+* Primero configurar el CLI de AWS.
+
+```terminal
+aws configure
+```
+
+* Primero hay que crear un IAM Role con permisos para EMR y S3.
+
+```terminal
+aws iam create-role \
+    --role-name EMRServerlessS3RuntimeRole \
+    --assume-role-policy-document file://emr-serverless-trust-policy.json
+```
+
+* Segundo, crear un IAM policy para la carga de trabajo. Este policy provee acceso de lectura a archivos guardados en S3 y acceso de lectura y escritura a archivos guardados en el bucket `amazon-reviews-eafit`.
+
+```terminal
+aws iam create-policy \
+    --policy-name EMRServerlessS3AndGlueAccessPolicy \
+    --policy-document file://emr-sample-access-policy.json
+```
+
+* Tercero, adjuntar el IAM policy `EMRServerlessS3AndGlueAccessPolicy` al job runtime role `EMRServerlessS3RuntimeRole`:
+
+```terminal
+aws iam attach-role-policy \
+    --role-name EMRServerlessS3RuntimeRole \
+    --policy-arn policy-arn
+```
+
+Donde `policy-arn` es el ARN del trust policy, es decir el ARN de `EMRServerlessS3RuntimeRole`.
+
+* Cuarto, crear una aplicación ser EMR Serverless:
+
+```terminal
+aws emr-serverless create-application \
+    --release-label emr-6.6.0 \
+    --type "SPARK" \
+    --name my-application
+```
+
+* Quinto, crear un job de EMR Serverless:
+
+```terminal
+aws emr-serverless start-job-run \
+    --application-id application-id \
+    --execution-role-arn job-role-arn \
+    --name job-run-name \
+    --job-driver '{
+        "sparkSubmit": {
+          "entryPoint": "s3://DOC-EXAMPLE-BUCKET/scripts/wordcount.py",
+          "entryPointArguments": ["s3://DOC-EXAMPLE-BUCKET/emr-serverless-spark/output"],
+          "sparkSubmitParameters": "--conf spark.executor.cores=1 --conf spark.executor.memory=4g --conf spark.driver.cores=1 --conf spark.driver.memory=4g --conf spark.executor.instances=1"
+        }
+    }'
+```
--- recommender.py
+++ recommender.py
@@ -3,41 +3,43 @@ from pyspark.ml.recommendation import ALS
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Collaborative Filtering").getOrCreate()  # type: ignore
-data = spark.read.option("header", "true").csv("s3://amazon-reviews-eafit/*.csv")
 
-# Convert relevant columns to integer type
-data = (
-    data.withColumn("customer_id", data["customer_id"].cast("int"))
-    .withColumn("product_id", data["product_id"].cast("int"))
-    .withColumn("star_rating", data["star_rating"].cast("float"))
-)
+if __name__ == "__main__":
+    data = spark.read.csv("s3://amazon-reviews-eafit/*.tsv", sep=r"\t", header=True)
 
-# Split data into training and test sets
-(training, test) = data.randomSplit([0.8, 0.2])
+    # Convert relevant columns to integer type
+    data = (
+        data.withColumn("customer_id", data["customer_id"].cast("int"))
+        .withColumn("product_id", data["product_id"].cast("int"))
+        .withColumn("star_rating", data["star_rating"].cast("float"))
+    )
 
-# Build the recommendation model using ALS on the training data
-als = ALS(
-    maxIter=5,
-    regParam=0.01,
-    userCol="customer_id",
-    itemCol="product_id",
-    ratingCol="star_rating",
-)
-model = als.fit(training)
+    # Split data into training and test sets
+    (training, test) = data.randomSplit([0.8, 0.2])
 
-# Evaluate the model by computing the RMSE on the test data
-predictions = model.transform(test)
-evaluator = RegressionEvaluator(
-    metricName="rmse", labelCol="star_rating", predictionCol="prediction"
-)
-rmse = evaluator.evaluate(predictions)
-print(f"Root-mean-square error = {rmse}")
+    # Build the recommendation model using ALS on the training data
+    als = ALS(
+        maxIter=5,
+        regParam=0.01,
+        userCol="customer_id",
+        itemCol="product_id",
+        ratingCol="star_rating",
+    )
+    model = als.fit(training)
 
-# Generate top 10 product recommendations for each user
-userRecs = model.recommendForAllUsers(10)
+    # Evaluate the model by computing the RMSE on the test data
+    predictions = model.transform(test)
+    evaluator = RegressionEvaluator(
+        metricName="rmse", labelCol="star_rating", predictionCol="prediction"
+    )
+    rmse = evaluator.evaluate(predictions)
+    print(f"Root-mean-square error = {rmse}")
 
-# Show top 10 recommendations for each user
-print(userRecs.show())
+    # Generate top 10 product recommendations for each user
+    userRecs = model.recommendForAllUsers(10)
 
-# Stop SparkSession
-spark.stop()
+    # Show top 10 recommendations for each user
+    print(userRecs.show())
+
+    # Stop SparkSession
+    spark.stop()
