19458e436c8d47f5deecaffe337a1c19a4f9b1c6
David Armendariz <david.adrian.a.p@gmail.com>
2024-04-27 22:01:10 -0500

    Adds calculate_rows_per_category.py

---
+++ calculate_rows_per_category.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder.appName("Calculate rows per category")  # type: ignore
+    .config("fs.s3a.endpoint", "s3.us-east-2.amazonaws.com")
+    .config(
+        "fs.s3a.aws.credentials.provider",
+        "com.amazonaws.auth.DefaultAWSCredentialsProviderChain",
+    )
+    .getOrCreate()
+)
+
+if __name__ == "__main__":
+    data = spark.read.csv(
+        "s3a://amazon-reviews-eafit/data/*.tsv", sep=r"\t", header=True
+    )
+
+    # Perform groupBy operation on the "category" column
+    grouped_data = data.groupBy("category").count()
+
+    # Save the result to a CSV file in S3
+    grouped_data.write.csv(
+        "s3a://amazon-reviews-eafit/output/counts_per_category", mode="overwrite"
+    )
+
+    # Stop SparkSession
+    spark.stop()
