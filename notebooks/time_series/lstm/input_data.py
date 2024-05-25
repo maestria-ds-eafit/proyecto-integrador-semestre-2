ca573c954b068efff8796df15659ea00e605a7a4
Camilovelez1 <camilo95022@gmail.com>
2024-05-25 17:29:47 -0500

    add lstm

--- notebooks/time_series/lstm/input_data.py
+++ notebooks/time_series/lstm/input_data.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 class InputData:
     def __init__(
         self,
-        data: pd.DataFrame,
+        df: pd.DataFrame,
         sequence_length: int,
         train_size_proportion=0.8,
         scaler: MinMaxScaler | StandardScaler | None = None,
@@ -14,15 +14,12 @@ class InputData:
         self.sequence_length = sequence_length
         self.train_size_proportion = train_size_proportion
         self.scaler = scaler
-        self.data = data
+        self.df = df[["gap"]]
+        self.read_data()
         self.scale_data()
 
     def read_data(self):
-        df = pd.read_csv(self.data_path)
-        df["Date"] = pd.to_datetime(df["Date"])
-        df.set_index("Date", inplace=True)
-        self.df = df
-        self.data = df["Incoming Examinations"].to_numpy().reshape(-1, 1)
+        self.data = self.df["gap"].to_numpy().reshape(-1, 1)
 
     def scale_data(self):
         if not self.scaler:
