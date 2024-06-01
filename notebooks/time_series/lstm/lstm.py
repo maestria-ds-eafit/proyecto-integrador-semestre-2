c9260134e46112cbd257462d2481a4537a03c42e
David Armendariz <david.adrian.a.p@gmail.com>
2024-06-01 14:26:25 -0500

    Adds random seed

--- notebooks/time_series/lstm/lstm.py
+++ notebooks/time_series/lstm/lstm.py
@@ -1,9 +1,12 @@
+import torch
 import torch.nn as nn
 
 
 class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, random_seed=42):
         super(LSTM, self).__init__()
+        if random_seed:
+            torch.manual_seed(random_seed)
         self.rnn = nn.LSTM(input_size, hidden_size, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
